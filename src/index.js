f2e405e3f2ebacf5ed90d349836835b1c705a0df
DouglasTeles <douglastelesoliveira@gmail.com>
2021-07-26 23:40:58 -0300

    update

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json())
 const routes = require('./routes')
 app.use(routes)
 
-
-app.listen(process.env.PORT ||3000,()=>{
+const PORT = process.env.PORT||3000
+app.listen(PORT ,()=>{
     console.log("Aplicação rodando")
 })
